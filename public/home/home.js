c181cb06c3a9b8c8d41b67549403b3ffff64bc50
davibelini <dev.dado.belini@gmail.com>
2020-12-27 17:09:14 -0300

    sub header

--- public/home/home.js
+++ public/home/home.js
@@ -1,2 +1,10 @@
-function contactModal() {
+function typewriter(el) {
+  const textoArray = el.innerHTML.split('')
+  el.innerHTML = ''
+  textoArray.forEach((letter, i) => {
+    setTimeout(() => {
+      el.innerHTML += letter
+    }, i * 30);
+  });
 }
+typewriter(document.querySelectorAll('.title')[1])
